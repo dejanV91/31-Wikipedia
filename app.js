3a7e2752c7430e7dbc297ed9cb84b0a97303447a
dejanV91 <dejanvukoicic91@gmail.com>
2022-09-09 20:08:59 +0200

    initial commit

--- app.js
+++ app.js
@@ -6,20 +6,25 @@ const searchBtn = document.querySelector(".btn");
 const loading = document.querySelector(".loading-div");
 const searchDiv = document.querySelector(".search-div");
 const content = document.querySelector(".content-div");
+const message = document.querySelector(".message");
 
 searchDiv.addEventListener("submit", (e) =>{
     e.preventDefault();
-    loading.classList.add("show");
 
     const fetchWiki = async (search) => {
         try{
-            console.log(search);
+            content.innerHTML= ""
+            message.classList.remove("show")
+            loading.classList.add("show");
             const response = await fetch(`${url}${search}`);
             const data = await response.json();
             return data.query.search
         }
         catch { 
-            loading.innerHTML = "Please Enter Valid Search Term"
+            content.innerHTML= ""
+            loading.classList.remove("show");
+            message.classList.add("show")
+            message.innerHTML = "Please Enter Valid Search Term"
         }
 
     }
@@ -27,7 +32,7 @@ searchDiv.addEventListener("submit", (e) =>{
     const display = async () => {
         let value = input.value;
         const data = await fetchWiki(value);
-
+        if(data.length >= 1){
             content.innerHTML = data
                 .map((item) => {
                     const {title, snippet, pageid} = item;
@@ -39,7 +44,15 @@ searchDiv.addEventListener("submit", (e) =>{
                             </a>`
                 })
                 .join("");
-            loading.classList.remove("show");    
+            loading.classList.remove("show"); 
+            message.classList.remove("show");
+        }
+        else {
+            content.innerHTML = ""
+            loading.classList.remove("show"); 
+            message.innerHTML = "No Matching Results. Please Try Again."
+            message.classList.add("show")
+        }   
     }
 
     display();
